7b341393469547c70c87e8f5fb4445b59784fa99
1078315905@qq.com <1078315905@qq.com>
2019-03-25 10:14:45 +0800

    express模版

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
+var fileStreamRotator = require('file-stream-rotator');
 //解决跨域
 var CORS = require('cors')
 
@@ -17,7 +19,36 @@ app.set('view engine', 'pug');
 
 //解决跨域
 app.use(CORS());
-app.use(logger('dev'));
+
+
+//写日志
+//日志记录
+
+var logDirectory = path.join(__dirname, 'log');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+var accessLogStream = fileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: path.join(logDirectory, 'access-%DATE%.log'),
+  frequency: 'daily',
+  verbose: false
+})
+
+//制作自定义token
+logger.token('ips',function(req, res) {
+  return req.ip||'-';
+})
+
+logger.token('from',function(req, res) {
+  return req.query.from||'-';
+})
+//制作自定义日志格式
+logger.format('gsluo','[gsluo] :method :url :status :res[content-length] - :response-time ms  :ips :from');
+//  解决跨域问题
+app.use(cors());
+// 写日志
+// app.use(logger('dev'));
+app.use(logger('gsluo',{stream: accessLogStream}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
